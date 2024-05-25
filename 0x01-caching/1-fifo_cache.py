a3661c02c42ca0bbb9f29037f483b474266ebc05
nguwenezapchristian <nguwenezacc982@gmail.com>
2024-05-25 14:13:31 +0200

    fifo cache

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" FIFO Caching Module """
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    a class FIFOCache that inherits from
+    BaseCaching and is a caching system
+    """
+
+    def __init__(self):
+        """ Initializes the class """
+        super().__init__()
+        self.cache = []
+
+    def put(self, key, item):
+        """
+        assign to the dictionary self.cache_data
+        the item value for the key key
+        """
+        if key is None or item is None:
+            return
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            firts_item_key = self.cache.pop(0)
+            del self.cache_data[firts_item_key]
+            print(f"DISCARD: {firts_item_key}")
+        self.cache_data[key] = item
+        self.cache.append(key)
+
+    def get(self, key):
+        """return the value in self.cache_data linked to key"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
