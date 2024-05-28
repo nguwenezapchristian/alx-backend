b153b82ed39a71daefef7993925073a459e97426
nguwenezapchristian <nguwenezacc982@gmail.com>
2024-05-28 15:54:27 +0200

    Check if the Babel object has the localeselector method

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,42 +1,56 @@
 #!/usr/bin/env python3
 """
-Basic Babel setup implementation
+Basic Babel setup implementation.
 """
 from flask import Flask, render_template, request
 from flask_babel import Babel, _
 
-
 class Config:
     """
-    In order to configure available languages in our app, you will create
-    a Config class that has a LANGUAGES class attribute equal to ["en", "fr"]
+    Configuration class for Flask application.
+    Contains language and timezone settings.
     """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
-
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
+""" Check if the Babel object has the localeselector method """
+if hasattr(babel, 'localeselector'):
+    @babel.localeselector
+    def get_locale() -> str:
+        """
+        Get the best match locale from the request.
 
-@babel.localeselector
-def get_locale():
-    """ Get locale from request """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+        Returns:
+            str: Best match language.
+        """
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
+else:
+    """ Fallback to setting the locale selector function manually """
+    def get_locale() -> str:
+        """
+        Get the best match locale from the request.
 
+        Returns:
+            str: Best match language.
+        """
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+    babel.localeselector_func = get_locale
 
 @app.route('/', strict_slashes=False)
-def hello():
+def hello() -> str:
     """
-    Render the index.html template for the root URL.
+    Render the 2-index.html template for the root URL.
 
     Returns:
-        str: Rendered HTML of the index.html template.
+        str: Rendered HTML of the 2-index.html template.
     """
     return render_template("2-index.html")
-
 
 if __name__ == "__main__":
     """
