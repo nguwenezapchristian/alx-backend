7801c4d1d6b65bead3d2af1775f24855d12059c2
nguwenezapchristian <nguwenezacc982@gmail.com>
2024-05-28 17:41:28 +0200

    removed pycodestyle error

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -32,6 +32,7 @@ def hello() -> str:
     """
     return render_template("4-index.html")
 
+
 @babel.localeselector
 def get_locale() -> str:
     """ Get locale from request """
@@ -43,6 +44,7 @@ def get_locale() -> str:
             return locale
     """if not supported use the default """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 if __name__ == "__main__":
     """
